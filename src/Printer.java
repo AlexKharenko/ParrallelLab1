e19991a0aa508cf9572b32f49650f7d569ffa636
AlexKharenko <alexey16052002@gmail.com>
2022-02-16 17:04:33 +0200

    Add wait and notify

--- src/Printer.java
+++ src/Printer.java
@@ -10,16 +10,14 @@ public class Printer {
         line_count++;
     }
 
-    public synchronized void printSymbolsSync(Symbol s) {
+    public synchronized void printSymbolsSync(Symbol s) throws InterruptedException {
         if (line_count % MaxSymbLine == 0 && line_count != 0) {
             System.out.print('\n');
         }
         s.printSymbol();
+        notify();
 
         line_count++;
-        try {
-            Thread.sleep(2);
-        } catch (InterruptedException e) {
-        }
+        wait();
     }
 }
